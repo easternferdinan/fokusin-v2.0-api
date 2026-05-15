b6600e861a50cba48fc39243c08b38cc91a9a05f
easternferdinan <ferdinandrestu03@gmail.com>
2026-05-15 13:19:43 +0700

    chore (api): add custom db operation error handler

---
+++ core/exceptions.py
@@ -0,0 +1,2 @@
+class DatabaseOperationError(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from api.v1.router import api_router
+from core.exceptions import DatabaseOperationError
 
-app = FastAPI()
+app = FastAPI(title="Fokusin RESTful API")
 
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Welcome to Fokusin RESTful API. Please use /docs for API documentation."}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.exception_handler(DatabaseOperationError)
+async def database_operation_error_handler(request: Request, exc: DatabaseOperationError):
+    """
+    Handle DatabaseOperationError exception into HTTP 500 response.
+    """
+    print(f"Database Operation Error: {exc.__cause__ or 'Unknown error'}")
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            "detail": str(exc)
+        }
+    )
