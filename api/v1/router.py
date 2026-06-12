3913383c2aee176da874dbf8868478b7a5b9d929
easternferdinan <ferdinandrestu03@gmail.com>
2026-06-12 15:07:03 +0700

    fix(api): handle missing endpoint

--- api/v1/router.py
+++ api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.v1.endpoints import auth, dashboard, tasks, pomodoro, notification, stress_analysis, log, admin, super_admin
+from api.v1.endpoints import auth, dashboard, tasks, pomodoro, notification, stress_analysis, report, log, admin, super_admin
 
 api_router = APIRouter()
 
@@ -9,6 +9,7 @@ api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 api_router.include_router(pomodoro.router, prefix="/pomodoro", tags=["pomodoro"])
 api_router.include_router(notification.router, prefix="/notifications", tags=["notifications"])
 api_router.include_router(stress_analysis.router, prefix="/analysis", tags=["stress analysis"])
+api_router.include_router(report.router, prefix="/report", tags=["report"])
 api_router.include_router(log.router, prefix="/logs", tags=["logs"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(super_admin.router, prefix="/super-admin", tags=["super admin"])
---
+++ schemas/report.py
@@ -0,0 +1,23 @@
+from enums.report_enums import ReportRecommendationColorLabelEnum
+from pydantic import BaseModel
+
+from schemas.stress_analysis import StressAnalysisResponse
+
+class StressTrendResponse(BaseModel):
+    labels: list[str]
+    values: list[float]
+
+class PotentialStressFactorsResponse(BaseModel):
+    deadline_is_tomorrow_tasks: str
+    piling_up_tasks: str
+    sleep_quality: str
+
+class RecommendationResponse(BaseModel):
+    subject: str
+    color_label: ReportRecommendationColorLabelEnum 
+    messages: list[str]
+
+class StressReportResponse(BaseModel):
+    all_stress_analysis: list[StressAnalysisResponse]
+    potential_stress_factors: PotentialStressFactorsResponse
+    recommendations: list[RecommendationResponse]
